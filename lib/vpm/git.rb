fb9a5bf74fcdd5e0d944c546e53beb05d21cdde5
Jingwen Owen Ou <jingweno@gmail.com>
2012-01-17 21:59:32 -0800

    Make it less verbose for checking command status

--- lib/vpm/git.rb
+++ lib/vpm/git.rb
@@ -2,12 +2,12 @@ module VPM
   module Git
     def self.clone(repo, dir)
       `git clone #{repo} #{dir}`
-      $?.to_i == 0 ? true : false
+      $?.to_i == 0
     end
 
     def self.checkout_tag(tag)
       `git checkout -b #{tag} #{tag}`
-      $?.to_i == 0 ? true : false
+      $?.to_i == 0
     end
 
     def self.current_revision
